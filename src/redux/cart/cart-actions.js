7130e7dea8aeb156ed8dc1f158618f1d976209cd
Martina Carter <martinac@mail.usf.edu>
2021-05-31 18:43:48 -0400

    added cart utils to group items

--- src/redux/cart/cart-actions.js
+++ src/redux/cart/cart-actions.js
@@ -1,5 +1,11 @@
+import CartActionTypes from './cart-types'
 import CartActionTYpes from './cart-types'
 
 export const toggleCartHidden  = () =>({
   type: CartActionTYpes.TOGGLE_CART_HIDDEN
+})
+
+export const addItem = item =>({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item
 })
